b790d9028f81828503bfe6ee8469116fc41a282a
Calvin Cheng <linchuan.cheng@gmail.com>
2013-10-24 11:51:31 +0800

    Update template with nice styling from MOT theme

--- main.go
+++ main.go
@@ -18,8 +18,10 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	http.HandleFunc("/", hello)
+    http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, r.URL.Path[1:])
+    })
 	if err := http.ListenAndServe(":5050", nil); err != nil {
 		panic(err)
 	}
 }
-
